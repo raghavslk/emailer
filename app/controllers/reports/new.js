abb3fa7c6456513d5347e6509bdb8b003570d43a
Raghav.Laxmikanth <Raghav.Laxmikanth>
2017-03-22 15:54:32 -0500

    Working emailer

--- app/controllers/reports/new.js
+++ app/controllers/reports/new.js
@@ -1,8 +1,11 @@
 import Ember from 'ember';
 import AWS from 'npm:aws-sdk';
+import config from '../../config/environment';
 
 export default Ember.Controller.extend({
 
+ accessKeyId: config.aws.accessKeyId,
+ secretAccessKey: config.aws.secretAccessKey,
  entry : {},
 
   actions: {
@@ -29,15 +32,21 @@ export default Ember.Controller.extend({
         ReplyToAddresses: [submittedForm.email],
         Source: `raghavslk@gmail.com`, // this has to be verified email in SES
       };
-      
-    /*  ses.sendEmail(emailParams, function(error, data) {
+      const ses = new AWS.SES({
+      apiVersion: '2010-12-01',
+      accessKeyId: this.get('accessKeyId'),
+      secretAccessKey: this.get('secretAccessKey'),
+
+      "region": "us-west-2"
+    });
+      ses.sendEmail(emailParams, function(error, data) {
         if (error) {
            console.log(error);
         } else {
            console.log('Report sent successfully');
         }
       });
-      */
+      
       console.log(submittedForm);
       console.log(ses);
     }
